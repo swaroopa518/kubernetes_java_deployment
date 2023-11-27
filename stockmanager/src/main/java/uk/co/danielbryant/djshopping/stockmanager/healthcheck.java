fca6a550b318ac11f13e63448d935ad56de44b16
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-27 16:46:05 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -11,3 +11,21 @@
 //         return "Hello User !! " + new Date();
 //     }
 // }
+
+package com.springboot.healthcheck;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class Runner {
+ 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+     
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+        LOGGER.info("Springboot actuator application is started successfully.");
+    }
+}
